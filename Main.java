ae4322245a0a82a9cc4f0592140f2345d103b1c0
GrantK <grantklees108@gmail.com>
2018-01-12 12:48:19 -0800

    Changed to arrays and text change
    attempting to change text display and went back to arrarys

--- Main.java
+++ Main.java
@@ -9,43 +9,65 @@ import java.util.*;
  */
 public class Main
 {
-    void bubbleSort(ArrayList<Integer> arr)
+    public static void bubbleSort(int arr[])
     {
-        int n = arr.size();
-        
+        int n = arr.length;
         printArray(arr);
         System.out.println();
         for (int i = 0; i < n-1; i++)
             for (int j = 0; j < n-i-1; j++)
-                if (arr.get(j) > arr.get(j+1))
+                if (arr[j] > arr[j+1])
                 {
                     // swap temp and arr[i]
-                    int temp = arr.get(j);
-                    arr.set(j,j+1);
-                    arr.set(j+1,temp);
-                    printArray(arr.subList(1, 4));
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    printPartialArray(arr, j, j+1);
                     System.out.println();
                 }
     }
  
     /* Prints the array */
-    void printArray(List<Integer> arr)
+    public static void printArray(int arr[])
     {
-        int n = arr.size();
-        for (int i=0; i<n; ++i)
-            System.out.print(arr.get(i) + " ");
+        int n = arr.length;
+        for (int i=0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    
+    public static void printPartialArray(int arr[], int index1, int index2)
+    {
+        int n = arr.length;
+        for (int i=0; i < n; ++i)
+            if(i == index1)
+            {
+                System.out.print("\u001B[1m" + arr[i] + "\001B[0m ");
+            }else if(i == index2)
+            {
+                System.out.print("\033[1m" + arr[i] + "\033[0m ");
+            }else
+            System.out.print(arr[i] + " ");
         System.out.println();
     }
  
     // Driver method to test above
     public static void main(String args[])
     {
-        Main ob = new Main();
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        arr.add(26);
-        // 60, 14, 96, 29	62	76	95	28	89	76	64	58	38	84	23	75	41	29	66)
-        ob.bubbleSort(arr);
+        int arr[] = new int[20];
+        arr = gen();
+        bubbleSort(arr);
         System.out.println("Sorted array");
-        ob.printArray(arr);
+        printArray(arr);
+    }
+    
+    public static int[] gen()
+    {
+        int arr[] = new int[20];
+        for(int i = 0; i < 20; i++)
+            {
+                arr[i] = (((int)(Math.random()*100)) + 1);
+            }
+        return arr;
     }
 }
